b57a0c8c8ae72d683a244bbc9026babc6768e736
boodovskiy <dzak1987@mail.ru>
2024-12-09 17:51:46 +0200

    Get Strapi data for Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337"
+
+  try {
+    const response = await fetch(baseUrl + path);
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+
+  const { title, description } = strapiData.data;
+
   return (
     <main className="container mx-auto py-6">
-      <Button />
+      <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p>
     </main>
   );
 }
