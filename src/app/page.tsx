f0ad6e749ac00133c9ae018160b6abdd18448315
boodovskiy <dzak1987@mail.ru>
2024-12-13 11:47:17 +0200

    Install qs, add fethching data from blocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,37 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import qs from "qs";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      on:{
+        "layout.hero-section": {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"]
+              },
+              link: {
+                populate: true
+              }
+            }
+          }
+      }
+    }
+  },
+});
+
 
 async function getStrapiData(path: string) {
   const baseUrl = "http://localhost:1337"
 
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
+  console.log(url.href);
+
   try {
-    const response = await fetch(baseUrl + path);
+    const response = await fetch(url.href);
     const data = await response.json();
+    console.dir(data, { depth: null });
     return data;
 
   } catch (error) {
