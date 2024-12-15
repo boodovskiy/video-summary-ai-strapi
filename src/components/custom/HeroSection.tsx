4fc608d54d2c4883951e75e845ae3a6365c61e59
boodovskiy <dzak1987@mail.ru>
2024-12-15 15:54:04 +0200

    Replace HeroSection html with real data

--- src/components/custom/HeroSection.tsx
+++ src/components/custom/HeroSection.tsx
@@ -1,12 +1,37 @@
 import Link from "next/link"
 
-const HeroSection = ({ data }: { readonly data: any }) => {
+interface ImageProps {
+    id: number;
+    url: string;
+    alternativeText: string;
+}
+
+interface LinkProps {
+    id: number;
+    url: string;
+    text: string;
+}
+
+interface HeroSectionProps {
+    data: {
+        id: number;
+        __component: string;
+        heading: string;
+        subHeading: string;
+        image: ImageProps;
+        link: LinkProps;
+    }
+}
+
+const HeroSection = ({ data }: Readonly<HeroSectionProps> ) => {
     console.dir(data, { depth: null })
+    const { heading, subHeading, image, link } = data;
+    const imageUrl = "http://localhost:1337" + image.url;
 
   return (
     <header className="relative h-[600px] overflow-hidden">
         <img 
-            src="http://localhost:1337/uploads/pexels_mikael_blomkvist_6476587_3c4d09397e.jpg" 
+            src={imageUrl}
             alt="Background"
             className="absolute inset-0 object-cover w-full h-full"
             width={1920}
@@ -18,15 +43,15 @@ const HeroSection = ({ data }: { readonly data: any }) => {
         />
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white bg-black bg-opacity-20">
             <h1 className="text-4xl">
-                Summarize Your Videos
+                {heading}
             </h1>
             <p className="mt-4 text-lg md:text-xl lg:text-2xl">
-                Save time and get the key points from your videos
+                {subHeading}
             </p>
             <Link className="mt-8 inline-flex items-center justify-center px-6 py-3 text-base font-medium text-black bg-white rounded-md shadow hover:bg-gray-100"
-                href="/login"
+                href={link.url}
             >
-                Login
+                {link.text}
             </Link>
         </div>
     </header>
