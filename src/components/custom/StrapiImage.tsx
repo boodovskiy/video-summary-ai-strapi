659cf54407b87a3046e98ded70a02229d282e044
boodovskiy <dzak1987@mail.ru>
2024-12-16 11:58:27 +0200

    Add StrapiImage component

---
+++ src/components/custom/StrapiImage.tsx
@@ -0,0 +1,28 @@
+import { getStrapiMedia } from "@/lib/utils";
+import Image from "next/image";
+
+interface StrapiImageProps {
+    src: string;
+    alt: string;
+    height: number;
+    width: number;
+    className?: string;
+}
+
+const StrapiImage = ({ src, alt, height, width, className }: Readonly<StrapiImageProps>) => {
+    if (!src) return null;
+    const imageUrl = getStrapiMedia(src);
+    const imageFallback = `https://placeholder.co/${width}x${height}`;
+
+  return (
+    <Image 
+        src={imageUrl ?? imageFallback}
+        alt={alt}
+        height={height}
+        width={width}
+        className={className}
+    />
+  )
+}
+
+export default StrapiImage
