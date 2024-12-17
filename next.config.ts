62b123d4ef420a6fd83d32396b0f9da7502b04a9
boodovskiy <dzak1987@mail.ru>
2024-12-17 12:31:30 +0200

    Add placehold.co into NextConfig

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         port: "1337",
         pathname: "/uploads/**/*",
       },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
     ],
   },
 };
