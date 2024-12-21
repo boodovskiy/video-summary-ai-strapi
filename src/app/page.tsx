a392dd5708772730bcada6621640d7db89d64033
boodovskiy <dzak1987@mail.ru>
2024-12-21 12:46:47 +0200

    Refactor data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,14 @@
-import { getStrapiUrl } from "@/lib/utils";
 import FeaturesSection from "@/components/custom/FeaturesSection";
 import HeroSection from "@/components/custom/HeroSection";
-import qs from "qs";
+import { getHompePageData } from "@/data/loaders";
 
-const homePageQuery = qs.stringify({
-  populate: {
-    blocks: {
-      on: {
-        "layout.hero-section": {
-          populate: {
-            image: {
-              fields: ["url", "alternativeText"],
-            },
-            link: {
-              populate: true,
-            },
-          },
-        },
-        "layout.features-section": {
-          populate: {
-            features: {
-              populate: true,
-            },
-          },
-        },
-      },
-    },
-  },
-});
+
+export default async function Home() {
+  const strapiData = await getHompePageData();
+  const { blocks } = strapiData?.data || [];
+
+  return <main>{blocks.map((block: any) => blockRenderer(block))}</main>;
+}
 
 function blockRenderer(block: any) {
   switch (block.__component) {
@@ -39,39 +20,4 @@ function blockRenderer(block: any) {
     default:
       return null;
   }
-}
-
-
-async function getStrapiData(path: string) {
-  const baseUrl = getStrapiUrl();
-
-  const url = new URL(path, baseUrl);
-  url.search = homePageQuery;
-
-  console.log(url.href);
-
-  try {
-    const response = await fetch(url.href, { cache: "no-store" });
-    
-    const data = await response.json();
-    console.dir(data, { depth: null });
-    return data;
-
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-export default async function Home() {
-  const strapiData = await getStrapiData("/api/home-page");
-
-  const { blocks } = strapiData.data;
-
-  if (!blocks) return <div>No blocks found</div>;
-
-  return (
-    <main>
-      {blocks.map((block: any) => blockRenderer(block))}
-    </main>
-  );
 }
---
+++ src/data/loaders.ts
@@ -0,0 +1,56 @@
+import { getStrapiUrl } from "@/lib/utils";
+import qs from "qs";
+
+const baseUrl = getStrapiUrl();
+
+async function fetchData(url: string) {
+ const authToken = null; // to do
+ const headers = {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${authToken}`,
+    },
+ };
+
+ try {
+    const response = await fetch(url, authToken ? headers : {});
+    const data = await response.json();
+    return data;
+ } catch (error) {
+    console.error("Error fetching data", error);
+    throw error;
+ }
+}
+
+export async function getHompePageData() {
+    const url = new URL("/api/home-page", baseUrl);
+
+    url.search = qs.stringify({
+      populate: {
+        blocks: {
+          on: {
+            "layout.hero-section": {
+              populate: {
+                image: {
+                  fields: ["url", "alternativeText"],
+                },
+                link: {
+                  populate: true,
+                },
+              },
+            },
+            "layout.features-section": {
+              populate: {
+                features: {
+                  populate: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    return await fetchData(url.href);
+}
