40b265b97e1a0d66c40539f06f5e488775bb458c
boodovskiy <dzak1987@mail.ru>
2024-12-30 20:03:17 +0200

    Add display Strapi Errors

---
+++ src/components/custom/StrapiErrors.tsx
@@ -0,0 +1,11 @@
+interface StrapiErrorsPops {
+    message: string | null;
+    name: string;
+    status: string | null;
+}
+
+export function StrapiErrors( { error }: { readonly error: StrapiErrorsPops }) {
+    if (!error?.message) return null;
+
+    return <div className="text-pink-500 text-md italic py-2">{error.message}</div>;
+}
