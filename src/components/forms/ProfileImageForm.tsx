3751d3cf90cd4109562356c3e14945eba9a87c3f
boodovskiy <dzak1987@mail.ru>
2025-01-10 10:06:16 +0200

    Finalize Profile Image Upload form (add action, errors output)

--- src/components/forms/ProfileImageForm.tsx
+++ src/components/forms/ProfileImageForm.tsx
@@ -3,6 +3,10 @@
 import { cn } from "@/lib/utils";
 import ImagePicker from "../custom/ImagePicker";
 import { SubmitButton } from "../custom/SubmitButton";
+import { uploadProfileImageAction } from "@/data/actions/profile-actions";
+import { useActionState } from "react";
+import { ZodErrors } from "../custom/ZodErrors";
+import { StrapiErrors } from "../custom/StrapiErrors";
 
 interface ProfileImageFormProps {
     id: string;
@@ -24,9 +28,18 @@ export function ProfileImageForm({
     data: Readonly<ProfileImageFormProps>;
     className?: string;
 }){
+    const uploadProfileImageWithIdAction = uploadProfileImageAction.bind(
+        null,
+        data?.id,
+    );
+
+    const [formState, formAction] = useActionState(
+        uploadProfileImageWithIdAction,
+        initialState
+    );
 
     return (
-        <form className={cn("space-y-4", className)}>
+        <form className={cn("space-y-4", className)} action={formAction}>
             <div>
                 <ImagePicker 
                     id="image"
@@ -34,6 +47,8 @@ export function ProfileImageForm({
                     label="Profile Image"
                     defaultValue={data?.url || ""}
                 />
+                <ZodErrors error={formState?.zodErrors?.image} />
+                <StrapiErrors error={formState?.strapiErrors}/>
             </div>
             <div className="flex justify-end">
                 <SubmitButton text="Update Image" loadingText="Saving Image"/>
