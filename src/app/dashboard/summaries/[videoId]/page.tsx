f1c3ed61e8dec419ff148306f23ec7b70db741f7
boodovskiy <dzak1987@mail.ru>
2025-01-21 09:58:16 +0200

    Implement SummaryCardForm with Summary details preview and markdown

--- src/app/dashboard/summaries/[videoId]/page.tsx
+++ src/app/dashboard/summaries/[videoId]/page.tsx
@@ -1,15 +1,17 @@
+import { SummaryCardForm } from "@/components/forms/SummaryCardForm";
+import { getSummaryById } from "@/data/loaders";
+
 interface PraramsProps {
     params: {
         videoId: string;
     }
 }
 
-const SummaryCardRoute =async (props: Readonly<PraramsProps>) => {
+const SummaryCardRoute = async (props: Readonly<PraramsProps>) => {
     const params = await props?.params;
     const {videoId} = params;
-  return (
-    <p>Summary card with go here: {videoId}</p>
-  )
+    const data = await getSummaryById(videoId);
+  return <SummaryCardForm item={data.data} />
 }
 
 export default SummaryCardRoute
