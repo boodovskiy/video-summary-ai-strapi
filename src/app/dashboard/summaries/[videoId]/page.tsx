1be4ab9ee9ce6cb05139889a16eb182a14c73e61
boodovskiy <dzak1987@mail.ru>
2025-02-03 10:57:31 +0200

    Fix next-async-request-api for summary id page

--- src/app/dashboard/summaries/[videoId]/page.tsx
+++ src/app/dashboard/summaries/[videoId]/page.tsx
@@ -2,14 +2,15 @@ import { SummaryCardForm } from "@/components/forms/SummaryCardForm";
 import { getSummaryById } from "@/data/loaders";
 
 type PageProps = {
-    params: {
+    params: Promise<{
       videoId: string;
-    };
+    }>;
   };
 
-export default async function SummaryCardRoute({ params }: PageProps) {
-    const { videoId } = params;
-    const data = await getSummaryById(videoId);
-  
-    return <SummaryCardForm item={data.data} />;
-  }
+export default async function SummaryCardRoute(props: PageProps) {
+  const params = await props.params;
+  const { videoId } = params;
+  const data = await getSummaryById(videoId);
+
+  return <SummaryCardForm item={data.data} />;
+}
