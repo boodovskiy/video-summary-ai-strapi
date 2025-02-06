f0592b0b9ed50518be326ac51d509b8deb5c3c98
boodovskiy <dzak1987@mail.ru>
2025-02-06 10:00:45 +0200

    Test STRAPI_URL exposing

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,18 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      // {
-      //   protocol: "http",
-      //   hostname: "localhost",
-      //   port: "1337",
-      //   pathname: "/uploads/**/*",
-      // },
-      // {
-      //   protocol: "http",
-      //   hostname: "127.0.0.1",
-      //   port: "1337",
-      //   pathname: "/uploads/**/*",
-      // },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
       {
         protocol: "https",
         hostname: "placehold.co",
