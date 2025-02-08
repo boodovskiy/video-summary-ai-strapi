048a043fae4c4ae4ba896391ffdf80a6a42da264
boodovskiy <dzak1987@mail.ru>
2025-02-08 12:04:38 +0200

    Upgrade extractYouTubeId funtction to work with modern youtube url formats

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -26,10 +26,13 @@ export function extractYouTubeId(urlOrId: string): string | null {
   }
 
   // Regular expression for standart Youtube links
-  const regExpStandard = /youtube\.com\/watch\?v=(a-zA-Z0-9_-)+/;
+  const regExpStandard = /[?&]v=([a-zA-Z0-9_-]{11})/;
 
   // Regular expression for standart Youtube links
-  const regExpShorts  = /youtube\.com\/shorts\?v=(a-zA-Z0-9_-)+/;
+  const regExpShorts = /youtube\.com\/shorts\/([a-zA-Z0-9_-]{11})/;
+
+  // Regular expression for youtu.be short links
+  const regExpShortLink = /youtu\.be\/([a-zA-Z0-9_-]{11})/;
 
   // Check for standard YouTube link
   const matchStandard = urlOrId.match(regExpStandard);
@@ -40,7 +43,13 @@ export function extractYouTubeId(urlOrId: string): string | null {
   // Check for YouTube Shorts link
   const matchShorts  = urlOrId.match(regExpShorts);
   if (matchShorts ) {
-    return matchShorts [1];
+    return matchShorts[1];
+  }
+
+  // Check for youtu.be short link
+  const matchShortLink = urlOrId.match(regExpShortLink);
+  if (matchShortLink) {
+    return matchShortLink[1];
   }
 
   // Return null if results not found
