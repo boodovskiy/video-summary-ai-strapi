00b485c35a001b71b8a0f72f0d9299844fa5f8dd
boodovskiy <dzak1987@mail.ru>
2025-02-09 10:38:07 +0200

    Fix in extractYouTubeId function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getStrapiUrl() {
-  return process.env.NEXT_PUBLIC_STRAPI_URL ?? "http://127.0.0.1:1337";
+  return process.env.STRAPI_URL ?? "http://127.0.0.1:1337";
 }
 
 export function getStrapiMedia(url: string | null) {
@@ -14,4 +14,44 @@ export function getStrapiMedia(url: string | null) {
   if (url.startsWith("data:")) return url;
   if (url.startsWith("http") || url.startsWith("//")) return url;
   return `${getStrapiUrl()}${url}`
+}
+
+export function extractYouTubeId(urlOrId: string): string | null {
+  // Regular expression for YouTube ID format
+  const regExpId = /^[a-zA-Z0-9_-]{11}$/;
+
+  // Check if the input is YouTubeId
+  if (regExpId.test(urlOrId)) {
+    return urlOrId;
+  }
+
+  // Regular expression for standart Youtube links
+  const regExpStandard = /[?&]v=([a-zA-Z0-9_-]{11})/;
+
+  // Regular expression for standart Youtube links
+  const regExpShorts = /youtube\.com\/shorts\/([a-zA-Z0-9_-]{11})/;
+
+  // Regular expression for youtu.be short links
+  const regExpShortLink = /youtu\.be\/([a-zA-Z0-9_-]{11})/;
+
+  // Check for standard YouTube link
+  const matchStandard = urlOrId.match(regExpStandard);
+  if (matchStandard) {
+    return matchStandard[1];
+  }
+
+  // Check for YouTube Shorts link
+  const matchShorts  = urlOrId.match(regExpShorts);
+  if (matchShorts ) {
+    return matchShorts[1];
+  }
+
+  // Check for youtu.be short link
+  const matchShortLink = urlOrId.match(regExpShortLink);
+  if (matchShortLink) {
+    return matchShortLink[1];
+  }
+
+  // Return null if results not found
+  return null;
 }
